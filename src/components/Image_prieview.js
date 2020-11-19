ac4e0cfb01d42ed0a05e307cc2b645b14db3d5f9
tasty-bilder <aq7798@naver.com>
2020-11-19 14:10:21 +0900

    Init

--- src/components/Image_prieview.js
+++ src/components/Image_prieview.js
@@ -8,7 +8,7 @@ class Image_prieview extends Component {
         <h2>여러분의 사진과 다양한 질의응답 을 통해 활동 점수를 분석합니다</h2>
         해당 테스트는 정확한 정보가 아닐 수 있습니다. 단순히 재미를 위해 즐겨주세요<p></p><br></br>
         해당 사이트는 계속해서 업데이트 중입니다.
-        <img src= {this.props.image} className="upimage" alt="image"></img>
+        <img src= {this.props.image} className="upimage" alt="user_selected_image"></img>
       </div>
     );
   }
